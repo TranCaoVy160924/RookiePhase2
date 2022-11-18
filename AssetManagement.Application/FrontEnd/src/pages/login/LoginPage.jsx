def9a9a79cbb27217e07f4ee6c194ee0be6e384c
Kossmin <phamsn2001@gmail.com>
2022-11-18 18:20:05 +0700

    Add validation for login form

--- AssetManagement.Application/FrontEnd/src/pages/login/LoginPage.jsx
+++ AssetManagement.Application/FrontEnd/src/pages/login/LoginPage.jsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { useLogin, useNotify } from 'react-admin';
+import React, { useState } from 'react';
+import { Form, TextInput, useLogin, useNotify, required } from 'react-admin';
 import { Avatar, Button, Box, TextField, CssBaseline, Typography, Container } from '@mui/material';
 import { createTheme, ThemeProvider, responsiveFontSizes, unstable_createMuiStrictModeTheme } from '@mui/material/styles';
 import { Formik } from "formik"
@@ -7,26 +7,34 @@ import * as Yup from 'yup';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 const LoginPage = () => {
+    const [isValid, setIsValid] = useState(true);
+
     const login = useLogin();
     const notify = useNotify();
     let theme = createTheme();
     theme = unstable_createMuiStrictModeTheme(theme);
 
-    const initialValues = {
-        userName : "",
-        password : ""
-    }
-    const loginSchema = Yup.object().shape({
-        userName : Yup.string().required("required"),
-        password : Yup.string().required("required"),
-    })
-
-    const handleFormSubmit = ({ userName, password }) => {
+    const handleFormSubmit = (data) => {
         // e.preventDefault();
-        login({ username: userName, password: password }).catch(() =>
+        console.log("hello");
+        login({ username: data.userName, password: data.password }).catch(() =>
             notify('Invalid email or password')
         );
     };
+
+    const requiredInput = (values) => {
+        const errors = {};
+        if (!values.userName) {
+            errors.userName = "This is required";
+            setIsValid(true);
+        } else if (!values.password) {
+            errors.password = "This is required";
+            setIsValid(true);
+        } else {
+            setIsValid(false);
+        }
+        return errors;
+    }
 
     return (
         <ThemeProvider theme={theme}>
@@ -47,59 +55,39 @@ const LoginPage = () => {
                         Log in
                     </Typography>
                     <Box sx={{ mt: 1 }}>
-                        <Formik
-                            validationSchema={loginSchema}
-                            initialValues={initialValues}
-                            onSubmit={handleFormSubmit}
-                        >
-                            {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
-                                <form onSubmit={handleSubmit}>
-                                    <Box
-                                        display="grid"
-                                        gap="30px"
-                                        gridTemplateColumns="repeat(5, minmax(0, 5fr))"
-                                    >
-                                        <TextField
-                                            margin="0px"
-                                            required
-                                            fullWidth
-                                            id="userName"
-                                            label="User Name"
-                                            name="userName"
-                                            autoComplete="current-userName"
-                                            autoFocus
-                                            onBlur={handleBlur}
-                                            onChange={handleChange}
-                                            defaultValue={values.userName}
-                                            error={ !!touched.userName && !!errors.userName }
-                                            helperText={ touched.userName && errors.userName }
-                                            sx={{ gridColumn: "span 5" }}
-                                        />
-                                        <TextField
-                                            margin="0px"
-                                            required
-                                            fullWidth
-                                            id="password"
-                                            label="Password"
-                                            name="password"
-                                            type="password"
-                                            autoComplete="current-password"
-                                            onBlur={handleBlur}
-                                            onChange={handleChange}
-                                            defaultValue={values.password}
-                                            error={ !!touched.password && !!errors.password }
-                                            helperText={ touched.password && errors.password }
-                                            sx={{ gridColumn: "span 5" }}
-                                        />
-                                    </Box>
-                                    <Box display="flex" justifyContent="end" mt="20px">
-                                        <Button type="submit" color="secondary" variant="contained">
-                                            Log in
-                                        </Button>
-                                    </Box>
-                                </form>
-                            )}
-                        </Formik>
+                        <Form mode='onBlur' validate={requiredInput} onSubmit={handleFormSubmit}>
+                            <Box
+                                display="grid"
+                                gap="30px"
+                                gridTemplateColumns="repeat(5, minmax(0, 5fr))"
+                            >
+                                <TextInput
+                                    fullWidth
+                                    id="userName"
+                                    label="User Name"
+                                    name="userName"
+                                    autoComplete="current-userName"
+                                    autoFocus
+                                    sx={{ gridColumn: "span 5" }}
+                                    source="username"
+                                />
+                                <TextInput
+                                    fullWidth
+                                    id="password"
+                                    label="Password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    sx={{ gridColumn: "span 5" }}
+                                    source="password"
+                                />
+                            </Box>
+                            <Box display="flex" justifyContent="end" mt="20px">
+                                <Button type="submit" color="secondary" variant="contained" disabled={isValid}>
+                                    Log in
+                                </Button>
+                            </Box>
+                        </Form>
                     </Box>
                 </Box>
             </Container>
