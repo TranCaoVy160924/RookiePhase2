f145cb8c4e0177488f07dc44c1560e94dfb6a422
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2022-11-21 18:57:50 +0700

    Add delete API and unit test class

---
+++ AssetManagement.Application.Tests/AssetControllerTest.cs
@@ -0,0 +1,57 @@
+﻿using AssetManagement.Data.EF;
+using AssetManagement.Domain.Enums.Asset;
+using AssetManagement.Domain.Models;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetManagement.Application.Tests
+{
+    public class AssetControllerTest
+    {
+        private readonly DbContextOptions _options;
+        private readonly AssetManagementDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _config;
+        private List<Asset> _assets;
+
+        public AssetControllerTest()
+        {
+            //Create InMemory dbcontext options
+            _options = new DbContextOptionsBuilder<AssetManagementDbContext>()
+                .UseInMemoryDatabase(databaseName: "AssetTestDb").Options;
+            //Create InMemory dbcontext with options
+            _context = new AssetManagementDbContext(_options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+
+            //Create fake data
+            SeedData();
+        }
+
+        private void SeedData()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                _context.Assets.Add(new Asset
+                {
+                    Id = i,
+                    Name = "Asset " + i,
+                    AssetCode = i.ToString(),
+                    Specification = i.ToString(),
+                    InstalledDate = DateTime.Now,
+                    State = i % 2 == 0 ? State.State1 : State.State2,
+                    IsActive = i % 2 == 0 ? true : false,
+                });
+            }
+
+            _context.SaveChanges();
+        }
+    }
+}
--- AssetManagement.Application/Controllers/AssetController.cs
+++ AssetManagement.Application/Controllers/AssetController.cs
@@ -1,12 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AssetManagement.Contracts.Asset.Request;
+using AssetManagement.Contracts.Common;
+using AssetManagement.Data.EF;
+using AssetManagement.Domain.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AssetManagement.Application.Controllers
 {
-    public class AssetController : Controller
+    [Route("api")]
+    [ApiController]
+    public class AssetController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly AssetManagementDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public AssetController(
+            AssetManagementDbContext dbContext,
+            IMapper mapper)
         {
-            return View();
+            _dbContext = dbContext;
+            _mapper = mapper;   
+        }
+
+        [HttpDelete("asset/delete")]
+        [Authorize]
+        public async Task<IActionResult> DeleteAsset(DeleteAssetRequest deleteAssetRequest)
+        {
+            Asset deletingAsset = await _dbContext.Assets
+                .Where(a => a.IsActive && a.Id == deleteAssetRequest.Id)
+                .FirstOrDefaultAsync();
+
+            try
+            {
+                if (deletingAsset != null)
+                {
+                    deletingAsset.IsActive = false;
+                    await _dbContext.SaveChangesAsync();
+                }
+                else
+                {
+                    throw new Exception("The asset does not exist");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ErrorResponseResult<string>(ex.Message));
+            }
+
+            return Ok(deletingAsset);
         }
     }
 }
