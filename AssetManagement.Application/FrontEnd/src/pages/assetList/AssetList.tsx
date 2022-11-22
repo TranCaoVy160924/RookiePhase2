a0427bff72e94ca4aeedb647eb376029e93ee66d
Kossmin <phamsn2001@gmail.com>
2022-11-22 18:41:40 +0700

    Build API, UI for view list

--- AssetManagement.Application/FrontEnd/src/pages/assetList/AssetList.tsx
+++ AssetManagement.Application/FrontEnd/src/pages/assetList/AssetList.tsx
@@ -1,8 +1,23 @@
 import React from "react"
-import { Datagrid, List, TextField } from "react-admin"
+import { Datagrid, List, SelectArrayInput, TextField, TextInput } from "react-admin"
 
 export default () => {
-   return <List>
+   const assetsFilter = [
+      <TextInput label="Search" source="searchString" alwaysOn />,
+      <SelectArrayInput source="states" choices={[
+         { id: '0', name: 'Available' },
+         { id: '1', name: 'Not available' },
+         { id: '2', name: 'Waiting for recycling' },
+         { id: '3', name: 'Recyled' },
+      ]} />,
+      <SelectArrayInput source="types" choices={[
+         { id: 'admin', name: 'Admin' },
+         { id: 'u001', name: 'Editor' },
+         { id: 'u002', name: 'Moderator' },
+         { id: 'u003', name: 'Reviewer' },
+      ]} />
+   ];
+   return <List filters={assetsFilter}>
       <Datagrid>
          <TextField source="id" />
          <TextField source="name" />
