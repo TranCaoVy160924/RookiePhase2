7abe542233d357032ef36f2081d6811d3abaaa80
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2022-11-24 15:20:49 +0700

    Update Test Case

--- AssetManagement.Contracts/AutoMapper/UserProfile.cs
+++ AssetManagement.Contracts/AutoMapper/UserProfile.cs
@@ -17,10 +17,13 @@ namespace AssetManagement.Contracts.AutoMapper
         {
             CreateMap<AppUser, UserResponse>();
             CreateMap<AssetManagement.Domain.Models.Asset, GetAssetByIdResponse>();
-            CreateMap<AssetManagement.Domain.Models.Asset, ViewListAssets_AssetResponse>().ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name));
+            CreateMap<AssetManagement.Domain.Models.Asset, ViewListAssets_AssetResponse>()
+                .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name));
             CreateMap<AssetManagement.Domain.Models.Asset, UpdateAssetResponse>();
             CreateMap<AssetManagement.Domain.Models.Asset, DeleteAssetReponse>();
-
+            CreateMap<AssetManagement.Domain.Models.Asset, GetAssetByIdResponse>();
+            CreateMap<AssetManagement.Domain.Models.Asset, CreateAssetRequest>();
+            CreateMap<CreateAssetRequest, AssetManagement.Domain.Models.Asset>();
         }
     }
 }
