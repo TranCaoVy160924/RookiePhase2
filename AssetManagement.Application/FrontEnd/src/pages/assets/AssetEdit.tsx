f58a215b412b2ba3ebead16237f87c41dc3e6f82
tmd <dattran2803@gmail.com>
2022-11-24 17:31:40 +0700

    update EditAsset frontend

--- AssetManagement.Application/FrontEnd/src/pages/assets/AssetEdit.tsx
+++ AssetManagement.Application/FrontEnd/src/pages/assets/AssetEdit.tsx
@@ -7,6 +7,7 @@ import * as assetService from '../../services/assets'
 import * as categoryService from '../../services/category'
 import CategorySelectBoxDisabled from '../../components/custom/CategorySelectBoxDisabled'
 import RadioButtonGroup from '../../components/custom/RadioButtonGroupInput'
+import AssetEditToolbar from '../../components/formToolBar/AssetEditToolbar'
 
 
 var today = new Date();
@@ -39,43 +40,6 @@ function EditAssetInformations() {
             .catch(error => console.log(error))
         }, [])
     console.log("asset", asset);
-    const handleFormSubmit = (data) => {
-        assetService.updateAsset(id,
-            {
-                name: data.name,
-                specification: data.specification,
-                installedDate: data.installedDate,
-                state: parseInt(data.state),
-
-            })
-            // Redirect to CategoryList
-            .then(reponseData => navigate("/assets"))
-            // Console log error
-            .catch(error => console.log(error))
-    };
-
-    const requiredInput = (values) => {
-        const errors = {
-            name: "",
-            specification: "",
-            installedDate: "",
-            state: ""
-        };
-        if (!values.name) {
-            errors.name = "This is required";
-            setIsValid(true);
-        } else if (!values.specification) { 
-            errors.specification = "This is required";
-            setIsValid(true);
-        } else if (!values.state) {
-            errors.state = "This is required";
-            setIsValid(true);
-        } else {
-            setIsValid(false);
-            return {};
-        }
-        return errors;
-    };
 
     return (
         <ThemeProvider theme={theme}>
@@ -93,10 +57,10 @@ function EditAssetInformations() {
                     <Typography component="h3" variant="h5" color="#cf2338" pb="40px" fontWeight="bold">
                         Create New Asset
                     </Typography>
-            <Edit>
-                <SimpleForm>
+            <Edit redirect="show" sx={{".RaEdit-card" : {boxShadow:"none"},".RaEdit-main":{width:'750px'}}}>
+                <SimpleForm toolbar={<AssetEditToolbar /> } >
                     <Box
-                    sx={{ display: "flex", flexDirection: "row", width: "650px" }}
+                    sx={{ display: "flex", flexDirection: "row", width: "650px", marginTop: "10px"}}
                     >
                         <Typography
                         variant="h6"
@@ -118,28 +82,7 @@ function EditAssetInformations() {
                         />
                     </Box>
                     <Box
-                        style={{ display: "flex", flexDirection: "row", width: "650px" ,marginTop: "3px"}}
-                                    >
-                                    <Typography
-                                        variant="h6"
-                                        style={{
-                                            width: "220px",
-                                            margin: "0",
-                                            padding: "0",
-                                            alignSelf: "center"      
-                                        }}
-                                    >Category *</Typography>
-                                    {/* Custom Dropdown Selection (Category) */}
-                                    <CategorySelectBoxDisabled
-                                        source="category"
-                                        format={(formValue) => (Array.prototype.filter.bind(category)(item => item.id===formValue))["name"]}
-                                        parse=""
-                                        defaultValue={asset.categoryId}
-                                        disabled={true}
-                                    />
-                                    </Box>
-                    <Box
-                    style={{ display: "flex", flexDirection: "row", width: "650px" ,marginTop: "3px"}}>
+                    style={{ display: "flex", flexDirection: "row", width: "650px" ,marginTop: "10px"}}>
                         <Typography
                         variant="h6"
                         style={{
@@ -161,6 +104,27 @@ function EditAssetInformations() {
                         helperText={false}
                         />
                     </Box>
+                    <Box
+                    style={{ display: "flex", flexDirection: "row", width: "650px" ,marginTop: "10px"}} 
+                    >
+                        <Typography
+                        variant="h6"
+                        style={{
+                            width: "220px",
+                            margin: "0",
+                            padding: "0",
+                            alignSelf: "center"
+                        }}
+                        >Category *</Typography>
+                        <CategorySelectBoxDisabled
+                        source="category"
+                        format={(formValue) => (Array.prototype.filter.bind(category)(item => item.id===formValue))["name"]}
+                        parse=""
+                        defaultValue={asset.categoryId}
+                        disabled={true}
+                    />
+                    </Box>
+                    
                     <Box
                                     style={{ display: "flex", flexDirection: "row", width: "650px", marginTop: "10px"}}
                                 >
@@ -186,19 +150,20 @@ function EditAssetInformations() {
                                         defaultValue={ asset.specification}
                                         InputLabelProps={{ shrink: false }}
                                     />
-                                </Box>
-                                <Box 
-                                    style={{ display:"flex", flexDirection:"row", width:"650px" }}
-                                >
-                                    <Typography 
-                                        variant="h6"
-                                        style={{
-                                            width:"220px", 
-                                            margin:"0", 
-                                            padding:"0", 
-                                            alignSelf:"center"
-                                        }}
-                                    >State *</Typography>
+                    </Box>
+                    <Box 
+                        style={{ display:"flex", flexDirection:"row", width:"650px" , marginTop: "10px"}}
+                    >
+                        <Typography 
+                            variant="h6"
+                            style={{
+                                width:"220px", 
+                                margin:"0", 
+                                padding:"0", 
+                                alignSelf:"start",
+                                paddingTop: "10px"
+                            }}
+                        >State *</Typography>
                     <RadioButtonGroup  label="" row={false} style={{ width:"430px"}} source="state" choices={[
                         { id: 0, name: "Available" },
                         { id: 1, name: "Not available" },
@@ -210,177 +175,6 @@ function EditAssetInformations() {
             </Box>
             </Container>
         </ThemeProvider>
-        // <ThemeProvider theme={theme}>
-        //     <Container component="main">
-        //         <CssBaseline />
-        //         <Box
-        //             sx={{
-        //                 margin: "auto",
-        //                 marginTop: 8,
-        //                 display: 'flex',
-        //                 flexDirection: 'column',
-        //                 // alignItems: 'center',
-        //                 width: "650px"
-        //             }}
-        //         >
-        //             <Typography component="h3" variant="h5" color="#cf2338" pb="40px" fontWeight="bold">
-        //                 Update Asset Informations
-        //             </Typography>
-        //             <Box sx={{ mt: 1 }}>
-        //                 <Form validate={requiredInput} onSubmit={handleFormSubmit}>
-        //                     <Box
-        //                         display="grid"
-        //                         gap="30px"
-        //                     >
-                                // <Box
-                                //     sx={{ display: "flex", flexDirection: "row", width: "650px" }}
-                                // >
-                                //     <Typography
-                                //         variant="h6"
-                                //         style={{
-                                //             width: "220px",
-                                //             margin: "0",
-                                //             padding: "0",
-                                //             alignSelf: "center"
-                                //         }}
-                                //     >Name *</Typography>
-                                //     <TextInput
-                                //         fullWidth
-                                //         label={false}
-                                //         name="name"
-                                //         source="name"
-                                //         style={{ width: "430px", margin: "0", padding: "0" }}
-                                //         helperText={false}
-                                //         InputLabelProps={{ shrink: false }}
-                                //     />
-                                // </Box>
-
-                                // <Box
-                                //     style={{ display: "flex", flexDirection: "row", width: "650px" }}
-                                // >
-                                //     <Typography
-                                //         variant="h6"
-                                //         style={{
-                                //             width: "220px",
-                                //             margin: "0",
-                                //             padding: "0",
-                                //             alignSelf: "center"
-                                //         }}
-                                //     >Category *</Typography>
-                                //     {/* Custom Dropdown Selection (Category) */}
-                                //     <CategorySelectBoxDisabled
-                                //         source="category"
-                                //         format={(formValue) => (Array.prototype.filter.bind(category)(item => item.id===formValue))["name"]}
-                                //         parse=""
-                                //         defaultValue={asset.categoryId}
-                                //         disabled={true}
-                                //     />
-                                // </Box>
-
-                                // <Box
-                                //     style={{ display: "flex", flexDirection: "row", width: "650px" }}
-                                // >
-                                //     <Typography
-                                //         variant="h6"
-                                //         style={{
-                                //             width: "220px",
-                                //             margin: "0",
-                                //             padding: "0",
-                                //             alignSelf: "center"
-                                //         }}
-                                //     >Specification *</Typography>
-                                //     <TextInput
-                                //         label={false}
-                                //         fullWidth
-                                //         multiline
-                                //         rows="3"
-                                //         style={{ width: "430px" }}
-                                //         name="specification"
-                                //         source="specification"
-                                //         helperText={false}
-                                //         defaultValue={ asset.specification}
-                                //         InputLabelProps={{ shrink: false }}
-                                //     />
-                                // </Box>
-
-        //                         <Box
-        //                             style={{ display: "flex", flexDirection: "row", width: "650px" }}
-        //                         >
-        //                             <Typography
-        //                                 variant="h6"
-        //                                 style={{
-        //                                     width: "220px",
-        //                                     margin: "0",
-        //                                     padding: "0",
-        //                                     alignSelf: "center"
-        //                                 }}
-        //                             >Installed Date *</Typography>
-        //                             <DateInput
-        //                                 fullWidth
-        //                                 label=""
-        //                                 name="installedDate"
-        //                                 source="installedDate"
-        //                                 InputLabelProps={{shrink: false}}
-        //                                 onBlur={(e) => e.stopPropagation()}
-        //                                 style={{ width:"430px" }}
-        //                                 helperText={false}
-        //                                 defaultValue={asset.installedDate}
-        //                             />
-        //                         </Box>
-
-        //                         <Box
-        //                             style={{ display: "flex", flexDirection: "row", width: "650px" }}
-        //                         >
-        //                             <Typography
-        //                                 variant="h6"
-        //                                 style={{
-        //                                     width: "220px",
-        //                                     margin: "0",
-        //                                     padding: "0",
-        //                                     alignSelf: "center"
-        //                                 }}
-        //                             >State *</Typography>
-        //                             <RadioButtonGroupInput
-        //                                 label={false}
-        //                                 defaultValue={asset.state}
-        //                                 name="state"
-        //                                 source="state"
-        //                                 choices={[
-        //                                     { state_id: 0, state: "Available" }, 
-        //                                     { state_id: 1, state: "Not available" },
-        //                                     { state_id: 2, state: "WaitingForRecycling" }, 
-        //                                     { state_id: 3, state: "Recycled" }, 
-        //                                 ]}
-        //                                 row={false}
-        //                                 style={{ width:"430px"}}
-        //                                 optionText="state"
-        //                                 optionValue="state_id"
-        //                                 helperText={false}
-        //                             />
-        //                         </Box>
-        //                     </Box>
-        //                     <Box display="flex" justifyContent="end" mt="20px">
-        //                         <Button
-        //                             type="submit"
-        //                             variant="contained"
-        //                             disabled={isValid}
-        //                             style={{ margin: "10px", backgroundColor: "#cf2338", color: "#fff" }}
-        //                         >
-        //                             Save
-        //                         </Button>
-        //                         <Button
-        //                             variant="outlined"
-        //                             onClick={(e) => navigate("/assets")}
-        //                             style={{ margin: "10px", color: "gray" }}
-        //                         >
-        //                             Cancel
-        //                         </Button>
-        //                     </Box>
-        //                 </Form>
-        //             </Box>
-        //         </Box>
-        //     </Container>
-        // </ThemeProvider>
     )
 }
 
