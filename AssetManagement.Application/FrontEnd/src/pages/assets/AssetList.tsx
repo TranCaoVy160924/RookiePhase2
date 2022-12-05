a88b98ef405e4db19fcf3293446947c2b98ee5dd
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2022-12-05 17:20:37 +0700

    Prototype

--- AssetManagement.Application/FrontEnd/src/pages/assets/AssetList.tsx
+++ AssetManagement.Application/FrontEnd/src/pages/assets/AssetList.tsx
@@ -29,6 +29,7 @@ import { listStyle } from "../../styles/listStyle";
 
 export default () => {
     const [isOpened, setIsOpened] = useState(false);
+    const [deleting, setDeleting] = useState(false);
     const [record, setRecord] = useState();
     // const { data } = useGetList("category", { pagination: { page: 1, perPage: 99 } })
     const dataProvider = useDataProvider();
@@ -151,6 +152,8 @@ export default () => {
                                         confirmTitle="Are you sure?"
                                         confirmContent="Do you want to delete this asset?"
                                         mutationOptions={{ onSuccess: (data) => refresh() }}
+                                        isOpen={deleting}
+                                        setDeleting={setDeleting}
                                     />
                                 )
                             }
@@ -162,6 +165,8 @@ export default () => {
                                         confirmContent="Do you want to delete this asset?"
                                         mutationOptions={{ onSuccess: (data) => refresh() }}
                                         disabled={true}
+                                        isOpen={deleting}
+                                        setDeleting={setDeleting}
                                     />
                                 )
                             }
