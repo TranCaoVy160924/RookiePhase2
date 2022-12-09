6523d1ebfc19501422651eed43c086729c50e018
Quang-Dobe <tdquangcr@gmail.com>
2022-12-09 12:20:20 +0700

    Add API

--- AssetManagement.Application/Controllers/ReturnRequestController.cs
+++ AssetManagement.Application/Controllers/ReturnRequestController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace AssetManagement.Application.Controllers
 {
@@ -24,6 +25,32 @@ namespace AssetManagement.Application.Controllers
         {
             _dbContext = dbContext;
             _mapper = mapper;
+        }
+
+        [HttpPost("api/home/{id}")]
+        public async Task<IActionResult> CreateReturnRequest(int id)
+        {
+            string userName = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Name)?.Value;
+            AppUser currentUser = _dbContext.AppUsers.FirstOrDefault(x => x.UserName == userName);
+            if (currentUser == null)
+            {
+                return BadRequest(new ErrorResponseResult<string>("Invalid User"));
+            }
+            Assignment assignment = _dbContext.Assignments.Where(x => x.Id == id).FirstOrDefault();
+            if (assignment == null)
+            {
+                return BadRequest(new ErrorResponseResult<string>("Invalid Assignment"));
+            }
+            ReturnRequest returnRequest = _mapper.Map<ReturnRequest>(assignment);
+            returnRequest.AssignedBy = currentUser.Id;
+            returnRequest.AcceptedBy = null;
+            returnRequest.ReturnedDate = null;
+            returnRequest.State = Domain.Enums.ReturnRequest.State.WaitingForReturning;
+
+            assignment.State = Domain.Enums.Assignment.State.WaitingForReturning;
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(new SuccessResponseResult<string>("Create ReturningRequest successfully"));
         }
 
         [HttpDelete("{id}")]
---
+++ AssetManagement.Contracts/AutoMapper/ReturnRequestProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+namespace AssetManagement.Contracts.AutoMapper
+{
+    public class ReturnRequestProfile : Profile
+    {
+        public ReturnRequestProfile()
+        {
+            CreateMap<Domain.Models.Assignment, Domain.Models.ReturnRequest>()
+                .ForMember(src => src.AssignmentId, opt => opt.MapFrom(act => act.Id))
+                .ForMember(src => src.AssignedDate, opt => opt.MapFrom(act => act.AssignedDate));
+
+        }
+    }
+}
