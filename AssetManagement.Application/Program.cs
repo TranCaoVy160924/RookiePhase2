1f3ae6968f59dfd4ce6737abbe4d4ae3dc8d4aea
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2022-12-10 20:25:41 +0700

    update swagger

--- AssetManagement.Application/Program.cs
+++ AssetManagement.Application/Program.cs
@@ -73,7 +73,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger asset managment solution", Version = "v1" });
-
+    c.CustomSchemaIds(type => type.ToString());
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n
